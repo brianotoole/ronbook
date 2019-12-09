88559491c97de5c80f20286e4c0be06ed73046c4
brianotoole <botoole518@gmail.com>
2019-12-09 12:49:47 -0500

    add contact

--- templates/contact.php
+++ templates/contact.php
@@ -11,121 +11,14 @@ get_template_part('template-parts/hero', '');
 <!-- INTRO -->
 <div id="First-Section" class="section section-center-mobile intro first-section text-center">
     <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container container-narrow">
-        <h2>Our History</h2>
+        <h2>Our Office</h2>
         <div class="heading-underline"></div>
         <div class="heading-underline"></div>
-        <p>Since our beginnings in 1984 in South Florida, our focus has never changed -- <strong>serve our clients with trust and integrity</strong>.</p>
-        <p>Our team has one priority: getting the job done for our clients. We believe the best strategy is a reputation for going above and beyond. We have built reputation for becoming policy experts and for providing strategic direction that creates success for our clients.</p>
+        <p><strong>Ronald L. Book, P.A.</strong></p>
+        <p>18851 NE 29th Ave. STE 1010<br>Aventura, FL 33180</p>
+        <p>305.935.1866</p>
     </div>
 </div>
-
-
-
-<!-- AREAS OF PRACTICE -->
-<div class="section section-center-mobile contained-section bg-primary faded">
-    <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container">
-      <h5 class="heading-sub u-color-black">Areas of Practice</h5>
-      <h2 class="heading-main u-color-black">Influence for Lasting Change</h2>
-      <div class="services">
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-lobbying.svg">
-          <div class="service__title h4">Lobbying</div>
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-government.svg">
-          <div class="service__title h4">Government Affairs</div>
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-procurement.svg">
-          <div class="service__title h4">Procurement</div>
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-business.svg">
-          <div class="service__title h4">Business Development</div>
-        </div><!--/.service-->
-      </div><!--/.services-->
-      <a data-w-id="6b01e7aa-5773-fde8-2b75-70067b33ff2d" href="/blog" class="service__link underline-link w-inline-block">
-                <div>See More News</div>
-                <div class="underline-container">
-                    <div class="underline-track"></div>
-                    <div class="underline-progress"></div>
-                </div><!--/.underline-contatiner-->
-            </a><!--/.news__link-->
-    </div><!--/.container-->
-    <div data-w-id="eb932dbb-19c7-a38e-6f4a-e8d79560b11f" class="decoration ring testimonials-ring"></div>
-</div><!--/.section-->
-
-
-
-<!-- BY THE NUMBERS -->
-<div class="section section-center-mobile contained-section bg-primary">
-    <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container">
-      <h5 class="heading-sub u-color-white">Our Experience</h5>
-      <h2 class="heading-main u-color-white">By the Numbers</h2>
-    </div><!--/.container-->
-    <div class="numbers__wrapper">
-      <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="numbers__col numbers__col-image"></div>
-      <div class="numbers__col numbers__col-stats">
-      <div data-w-id="77549b59-a675-6031-e461-e93208aba3cd" class="container">
-        <div class="numbers__stat h2">50+</div>
-        <div class="numbers__stat-text">Years of Experience</div>
-        <div class="numbers__stat h2">2 States</div>
-        <div class="numbers__stat-text">Federal Relations</div>
-        <div class="numbers__stat h2">$100M+</div>
-        <div class="numbers__stat-text">Managed Campaigns</div>
-      </div><!--/.container-->
-      </div><!--/.numbers__col-stats-->
-    </div><!--/.numbers__wrapper-->
-</div><!--/.section-->
-
-
-
-<!-- TESTIMONIALS -->
-<div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="section section-center-mobile testimonials contained-section bg-primary faded">
-    <div class="container container-narrow">
-      <h5 class="heading-sub u-color-black">Relationships. Respect. Results.</h5>
-      <h3 class="heading-main u-color-black">We lead the way for clients who are key players in their fields.</h3>
-    </div><!--/.container-narrow-->
-    <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container">
-      <div class="services">
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-auto.png">
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-uber.png">
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon service__icon--smaller" src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-rays.png">
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-fhsaa.png">
-        </div><!--/.service-->
-      </div><!--/.services-->
-      <div class="testimonials__wrapper">
-        <h5 class="heading-sub u-color-black">Testimonials</h5>
-        <div data-delay="4000" data-animation="slide" data-autoplay="1" data-nav-spacing="7" data-duration="500" data-infinite="1" class="testimonial-slider w-slider">
-            <div class="w-slider-mask">
-                <div class="testimonial-slide w-slide">
-                    <h4 class="testimonial-text">“They strike the perfect balance between personable and professional.”</h4>
-                    <div>– Andrew Clinton, Patron</div>
-                </div>
-                <div class="testimonial-slide w-slide">
-                    <h4 class="testimonial-text">“The best for a reason – these guys know their stuff”</h4>
-                    <div>– Anabelle Porter, Patron</div>
-                </div>
-                <div class="testimonial-slide w-slide">
-                    <h4 class="testimonial-text">“The team assisted in all facets of our campaign and knocked it out of the park”</h4>
-                    <div>– Anwar Sing, Unvest</div>
-                </div>
-            </div>
-            <div class="slider-arrow slider-arrow-left w-slider-arrow-left"><img src="https://uploads-ssl.webflow.com/5d93b1aa23f45c2c9836b8b3/5d93b1aa23f45cf42c36b8f5_icon-arrow-right-white.svg" alt="" /></div>
-            <div class="slider-arrow w-slider-arrow-right"><img src="https://uploads-ssl.webflow.com/5d93b1aa23f45c2c9836b8b3/5d93b1aa23f45cf42c36b8f5_icon-arrow-right-white.svg" alt="" /></div>
-            <div class="slide-nav w-slider-nav w-round"></div>
-        </div>
-      </div><!--/.testimonials__wrapper-->
-    </div><!--/.container-->
-</div><!--/.section-->
-
 
 
 
@@ -137,15 +30,10 @@ get_template_part('template-parts/hero', '');
   <div class="feat-case-study__col">  
     <div class="feat-case-study__content">
       <div class="container container-narrow">
-        <h6 class="feat-case-study__ribbon">Featured Case Study</h6>
-        <h2 class="feat-case-study__title">Florida High School Athletic Association</h2>
-        <p class="feat-case-study__description">loremfjkdsj fklasdjfkadsjflkasdj fklsadjklgsdjagkjasgklsdajgklasdjgk</p>
+        <h2 class="feat-case-study__title">Let's Discuss Your Next Project</h2>
+        <p class="feat-case-study__description">Fill out the form below and someone from our team will be in touch as soon as possible.</p>
         <a data-w-id="6b01e7aa-5773-fde8-2b75-70067b33ff2d" href="#" class="feat-case-study__link underline-link w-inline-block">
-          <div>View Project</div>
-            <div class="underline-container">
-              <div class="underline-track underline-track-white"></div>
-              <div class="underline-progress underline-progress-white"></div>
-          </div>
+        <?php echo do_shortcode('[contact-form-7 id="100" title="Contact"]'); ?>
         </a>
       </div><!--/.container-->
     </div><!--/.feat-case-study__content-->
@@ -153,47 +41,6 @@ get_template_part('template-parts/hero', '');
 </div><!--/.section-->
 
 
-
-<div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="section section-center-mobile news">
-    <div class="container">
-        <h2 class="section-title">In the News</h2>
-        <div class="heading-underline"></div>
-        <div class="heading-underline"></div>
-        <div class="news__list">
-          <div class="news__item">
-            <h3 class="news__title">Suzanne Gilbert Appointed to American Bar Association's Standing Committee on the Federal Judiciary</h3>
-            <div class="news__ribbon">Press Release</div>
-          </div><!--/.news__item-->
-          <div class="news__item">
-            <h3 class="news__title">Suzanne Gilbert Appointed to American Bar Association's Standing Committee on the Federal Judiciary</h3>
-            <div class="news__ribbon">Press Release</div>
-          </div><!--/.news__item-->
-          <div class="news__item">
-            <h3 class="news__title">Suzanne Gilbert Appointed to American Bar Association's Standing Committee on the Federal Judiciary</h3>
-            <div class="news__ribbon">Press Release</div>
-          </div><!--/.news__item-->
-        </div><!--/.news__list-->
-        <div class="news__link-wrapper">
-            <a data-w-id="6b01e7aa-5773-fde8-2b75-70067b33ff2d" href="/blog" class="underline-link w-inline-block">
-                <div>See More News</div>
-                <div class="underline-container">
-                    <div class="underline-track"></div>
-                    <div class="underline-progress"></div>
-                </div><!--/.underline-contatiner-->
-            </a><!--/.news__link-->
-        </div><!--/.text-right-->
-    </div><!--/.container-->
-</div><!--/.section-->
-
-
-
-<div class="section section-center-mobile cta bg-primary overflow-hidden">
-  <div class="container">
-    <h2 class="cta">Let's Work Together</h3>
-    <a href="/contact" class="button button-secondary">Get In Touch</a>
-  </div><!--/.container-->
-  <div data-w-id="e243cfb7-fd9e-8a1d-fc6f-e14c8cd05fe7" class="decoration ring cta-ring"></div>
-</div><!--/.section-->
 
 
 
