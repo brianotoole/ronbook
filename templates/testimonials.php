4abd510a1e4933a0d43350a466f97d43cb6548d1
brianotoole <botoole518@gmail.com>
2019-12-09 12:40:10 -0500

    add testimonials temp

--- templates/testimonials.php
+++ templates/testimonials.php
@@ -8,83 +8,12 @@ get_template_part('template-parts/hero', '');
 ?>
 
 
-<!-- INTRO -->
-<div id="First-Section" class="section section-center-mobile intro first-section text-center">
-    <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container container-narrow">
-        <h2>Our History</h2>
-        <div class="heading-underline"></div>
-        <div class="heading-underline"></div>
-        <p>Since our beginnings in 1984 in South Florida, our focus has never changed -- <strong>serve our clients with trust and integrity</strong>.</p>
-        <p>Our team has one priority: getting the job done for our clients. We believe the best strategy is a reputation for going above and beyond. We have built reputation for becoming policy experts and for providing strategic direction that creates success for our clients.</p>
-    </div>
-</div>
-
-
-
-<!-- AREAS OF PRACTICE -->
-<div class="section section-center-mobile contained-section bg-primary faded">
-    <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container">
-      <h5 class="heading-sub u-color-black">Areas of Practice</h5>
-      <h2 class="heading-main u-color-black">Influence for Lasting Change</h2>
-      <div class="services">
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-lobbying.svg">
-          <div class="service__title h4">Lobbying</div>
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-government.svg">
-          <div class="service__title h4">Government Affairs</div>
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-procurement.svg">
-          <div class="service__title h4">Procurement</div>
-        </div><!--/.service-->
-        <div class="service">
-          <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/icon-business.svg">
-          <div class="service__title h4">Business Development</div>
-        </div><!--/.service-->
-      </div><!--/.services-->
-      <a data-w-id="6b01e7aa-5773-fde8-2b75-70067b33ff2d" href="/blog" class="service__link underline-link w-inline-block">
-                <div>See More News</div>
-                <div class="underline-container">
-                    <div class="underline-track"></div>
-                    <div class="underline-progress"></div>
-                </div><!--/.underline-contatiner-->
-            </a><!--/.news__link-->
-    </div><!--/.container-->
-    <div data-w-id="eb932dbb-19c7-a38e-6f4a-e8d79560b11f" class="decoration ring testimonials-ring"></div>
-</div><!--/.section-->
-
-
-
-<!-- BY THE NUMBERS -->
-<div class="section section-center-mobile contained-section bg-primary">
-    <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container">
-      <h5 class="heading-sub u-color-white">Our Experience</h5>
-      <h2 class="heading-main u-color-white">By the Numbers</h2>
-    </div><!--/.container-->
-    <div class="numbers__wrapper">
-      <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="numbers__col numbers__col-image"></div>
-      <div class="numbers__col numbers__col-stats">
-      <div data-w-id="77549b59-a675-6031-e461-e93208aba3cd" class="container">
-        <div class="numbers__stat h2">50+</div>
-        <div class="numbers__stat-text">Years of Experience</div>
-        <div class="numbers__stat h2">2 States</div>
-        <div class="numbers__stat-text">Federal Relations</div>
-        <div class="numbers__stat h2">$100M+</div>
-        <div class="numbers__stat-text">Managed Campaigns</div>
-      </div><!--/.container-->
-      </div><!--/.numbers__col-stats-->
-    </div><!--/.numbers__wrapper-->
-</div><!--/.section-->
-
-
-
-<!-- TESTIMONIALS -->
-<div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="section section-center-mobile testimonials contained-section bg-primary faded">
+<!-- FEATURED CLIENS -->
+<div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="section testimonials contained-section">
     <div class="container container-narrow">
-      <h5 class="heading-sub u-color-black">Relationships. Respect. Results.</h5>
-      <h3 class="heading-main u-color-black">We lead the way for clients who are key players in their fields.</h3>
+        <h2>Featured Clients</h2>
+        <div class="heading-underline"></div>
+        <div class="heading-underline"></div>
     </div><!--/.container-narrow-->
     <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container">
       <div class="services">
@@ -101,8 +30,17 @@ get_template_part('template-parts/hero', '');
           <img class="service__icon" src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-fhsaa.png">
         </div><!--/.service-->
       </div><!--/.services-->
-      <div class="testimonials__wrapper">
-        <h5 class="heading-sub u-color-black">Testimonials</h5>
+    </div><!--/.container-->
+</div><!--/.section-->
+
+<!-- TESTIMONIALS -->
+<div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="section testimonials contained-section bg-secondary faded">
+    <div class="container container-narrow">
+      <h5 class="heading-sub u-color-black">Testimonials</h5>
+    </div><!--/.container-narrow-->
+    <div data-w-id="562ba68e-0ae8-5582-dbff-17f74b7d21e2" class="container">
+
+    <div class="testimonials__wrapper">
         <div data-delay="4000" data-animation="slide" data-autoplay="1" data-nav-spacing="7" data-duration="500" data-infinite="1" class="testimonial-slider w-slider">
             <div class="w-slider-mask">
                 <div class="testimonial-slide w-slide">
@@ -123,9 +61,9 @@ get_template_part('template-parts/hero', '');
             <div class="slide-nav w-slider-nav w-round"></div>
         </div>
       </div><!--/.testimonials__wrapper-->
+    
     </div><!--/.container-->
 </div><!--/.section-->
-
 
 
 
